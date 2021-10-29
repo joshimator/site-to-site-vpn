34ffadfdc9c1b4810d2babab195d0cdf4ed1cbee
joshimator <92186478+joshimator@users.noreply.github.com>
2021-10-29 09:20:45 +0200

    Update vpnclient.sh

--- vpnclient.sh
+++ vpnclient.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 cd /etc/openvpn/
+rm server.conf
 echo IP der Server VM:
 read ip
 
-echo "mode p2p" > server.conf
-echo "remote $ip 1194" >> server.conf
-echo "dev tun" >> server.conf
-echo "proto udp" >> server.conf
-echo "ifconfig 192.168.0.2 192.168.0.1" >> server.conf
-echo "secret static.key" >> server.conf
-echo "ping 10" >> server.conf
-echo "ping-restart 180">> server.conf
-echo "ping-timer-rem" >> server.conf
-echo "verb 3" >> server.conf
-echo "mute 50" >> server.conf
+echo "mode p2p" > client.conf
+echo "remote $ip 1194" >> client.conf
+echo "dev tun" >> client.conf
+echo "proto udp" >> client.conf
+echo "ifconfig 192.168.0.2 192.168.0.1" >> client.conf
+echo "secret static.key" >> client.conf
+echo "ping 10" >> client.conf
+echo "ping-restart 180">> client.conf
+echo "ping-timer-rem" >> client.conf
+echo "verb 3" >> client.conf
+echo "mute 50" >> client.conf
 
 sleep 2
 reboot
